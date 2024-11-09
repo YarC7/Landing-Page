8024189e0d13cd6c27942b525a1c7371917721c1
cankkun <nguyenduccanh177@gmail.com>
2024-11-10 06:55:37 +0700

    conf(layout):Configure path of storeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 import { WixClientContextProvider } from "@/context/wixContext";
-import StoreProvider from "./storeProvider";
+import StoreProvider from "./StoreProvider";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
